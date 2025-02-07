92ddf4f88d877b6f2690dd05fc1ca957514f163b
Victor An <2378740+duoan@users.noreply.github.com>
2025-02-06 17:56:31 -0800

    Update training code for the loss function, and use dataloader

--- train_single.py
+++ train_single.py
@@ -3,7 +3,16 @@ import jax.numpy as jnp
 import optax
 import jax.random as random
 from flax.training import train_state
+import flax.linen as nn
+from regex import T
+from sympy import im
 from model import CausalGPT
+from data import TokenDataset, jnp_collate_fn
+from torch.utils.data import DataLoader
+import torch
+import tiktoken
+
+torch.manual_seed(0)
 
 
 def create_train_state(key, model, learning_rate, batch_size, seq_length):
@@ -19,10 +28,12 @@ def create_train_state(key, model, learning_rate, batch_size, seq_length):
 
 @jax.jit
 def train_step(state, batch):
+    ids, labels = batch
+
     def loss_fn(params):
-        logits = state.apply_fn(params, batch)
-        loss = jnp.mean(jnp.square(logits))
-        return loss
+        logits = state.apply_fn(params, ids)
+        loss = optax.softmax_cross_entropy_with_integer_labels(logits, labels)
+        return jnp.mean(loss)
 
     loss, grads = jax.value_and_grad(loss_fn)(state.params)
     state = state.apply_gradients(grads=grads)
@@ -31,34 +42,50 @@ def train_step(state, batch):
 
 @jax.jit
 def eval_step(state, batch):
-    logits = state.apply_fn(state.params, batch)
-    loss = jnp.mean(jnp.square(logits))
-    return loss
+    ids, labels = batch
+    logits = state.apply_fn(state.params, ids)
+    loss = optax.softmax_cross_entropy_with_integer_labels(logits, labels)
+    return jnp.mean(loss)
 
 
-def evaluate(state, eval_ds):
+def evaluate(state, valid_data):
     losses = []
-    for batch in eval_ds:
+    for step, batch in enumerate(valid_data):
         loss = eval_step(state, batch)
         losses.append(loss)
-    return jnp.mean(jnp.array(losses))
+        print(f"Evaluation step {step} / {len(valid_data_loader)}, loss: {loss:6f}")
+
+    return jnp.sum(jnp.array(losses))
+
+
+encoding = tiktoken.get_encoding("o200k_base")
+
+
+def generate(state, input_ids, max_length=10, seq_length=1024):
+    for _ in range(max_length):
+        logits = state.apply_fn(state.params, input_ids[:, -seq_length:])
+        next_token = jnp.argmax(logits, axis=-1)[:, -1]
+        input_ids = jnp.concatenate([input_ids, next_token[:, None]], axis=-1)
+    print("context:\n", encoding.decode(input_ids[0][:seq_length]))
+    print("generate:\n", encoding.decode(input_ids[0][-max_length:]))
 
 
 # Training setup
-batch_size = 8
+batch_size = 2
 seq_length = 128
-learning_rate = 1e-3
+learning_rate = 5e-4
 num_epochs = 10
+log_every = 10
 eval_every = 10
 
 # Initialize model and state
 key = random.PRNGKey(0)
 model = CausalGPT(
-    vocab_size=50257,
+    vocab_size=200019,
     embed_dim=768,
-    num_heads=12,
-    num_layers=12,
-    mlp_dim=3072,
+    num_heads=8,
+    num_layers=2,
+    mlp_dim=128,
     dropout=0.1,
 )
 
@@ -66,22 +93,45 @@ state = create_train_state(key, model, learning_rate, batch_size, seq_length)
 
 # Generate dummy data for testing
 key, train_key, eval_key = random.split(key, 3)
-train_data = [
-    random.randint(train_key, (batch_size, seq_length), 0, 50257) for _ in range(10)
-]
-eval_data = [
-    random.randint(eval_key, (batch_size, seq_length), 0, 50257) for _ in range(2)
-]
 
+train_data_loader = DataLoader(
+    TokenDataset("tokens_train.npy", seq_length),
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=jnp_collate_fn,
+)
+
+print(f"train_data_loader size: {len(train_data_loader):,}")
+
+valid_data_loader = DataLoader(
+    TokenDataset("tokens_valid.npy", seq_length),
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=jnp_collate_fn,
+)
+
+print(f"valid_data_loader size: {len(valid_data_loader):,}")
+
+total_steps = num_epochs * len(train_data_loader)
 # Training loop with evaluation
 for epoch in range(num_epochs):
-    for step, batch in enumerate(train_data):
+    for step, batch in enumerate(train_data_loader):
         state, train_loss = train_step(state, batch)
-
-        if (1 + step) % eval_every == 0:
-            eval_loss = evaluate(state, eval_data)
+        global_step = epoch * len(train_data_loader) + step
+        if (1 + global_step) % log_every == 0:
             print(
-                f"Epoch {epoch}, Step {step}, Train Loss: {train_loss:.4f}, Eval Loss: {eval_loss:.4f}"
+                f"Training epoch:{epoch} / {num_epochs}, "
+                f"Step:{global_step:,}/ {total_steps}, "
+                f"Train Loss:{train_loss:6f}"
             )
+            generate(state, batch[0], seq_length=seq_length)
+        # if (1 + global_step) % eval_every == 0:
+        #     eval_loss = evaluate(state, valid_data_loader)
+        #     print(
+        #         f"Training epoch:{epoch} / {num_epochs}, "
+        #         f"Step:{global_step:,}/ {total_steps}, "
+        #         f"Train Loss:{train_loss:6f}"
+        #         f"Eval Loss:{eval_loss:6f}"
+        #     )
 
 print("Training completed!")
